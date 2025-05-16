a4d399736fed8b9b36366f829df69f959380f0c4
VickyUA <vickyy379@gmail.com>
2025-05-16 23:59:57 +0300

    upd

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,13 +1,21 @@
 import css from "./Contact.module.css";
+import { FaUser } from "react-icons/fa";
+import { BiSolidPhone } from "react-icons/bi";
 
-export default function Contact({ name, number }) {
+export default function Contact({ name, number, id, onDelete }) {
   return (
     <div className={css.cardWrapper}>
       <div>
-        <p>{name}</p>
-        <p>{number}</p>
+        <p>
+          <FaUser size="14" color="rgb(9, 90, 33)" padding="10px" />
+          {name}
+        </p>
+        <p>
+          <BiSolidPhone size="18" color="rgb(9, 90, 33)" />
+          {number}
+        </p>
       </div>
-      <button type="button" className={css.btn}>
+      <button type="button" className={css.btn} onClick={() => onDelete(id)}>
         Delete
       </button>
     </div>
